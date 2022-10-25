e27236e98d7b0c048d7dd284ef9f0a7631081493
Max Grötsch <max-groetsch@gmx.de>
2022-10-25 11:08:02 +0200

    palindrom ok

--- 3/Palindrom/Palindrome.cpp
+++ 3/Palindrom/Palindrome.cpp
@@ -2,7 +2,7 @@
 
 using namespace std;
 
-static bool palindrome(int input);
+static int palindrome(int input);
 
 int main()
 {
@@ -11,25 +11,26 @@ int main()
 	cout << "Zahl eingeben";
 	cin >> input;
 	result = palindrome(input);
-	cout << "Die umgekehrte Zahl lautet: ";
+	cout << "Die umgekehrte Zahl lautet: " << result;
 	if (result == input)
 	{
-		cout << "Die zahl ist ein Palindrom";
+		cout << "\nDie zahl ist ein Palindrom";
 	}
 	else
 	{
-		cout << "Die Zahl ist kein Palindrom";
+		cout << "\nDie Zahl ist kein Palindrom";
 	}
 	return 0;
 }
 
-static bool palindrome(int input)
+static int palindrome(int input)
 {
-	bool result;
-	int digit;
-	while (input > 0)
+	int n = input, reversed = 0, remainder;
+	while (n != 0)
 	{
-		digit %= input;
+		remainder = n % 10;
+		reversed = reversed * 10 + remainder;
+		n /= 10;
 	}
-	return result;
+	return reversed;
 }
