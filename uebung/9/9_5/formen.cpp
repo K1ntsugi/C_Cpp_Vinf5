77fc09d19e69c488c78ea1088b65f8d888102641
Maximilian_Groetsch <max-groetsch@gmx.de>
2022-12-22 13:58:18 +0100

    ok

---
+++ uebung/9/9_5/formen.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+using namespace std;
+
+class Figur
+{
+private:
+    /* data */
+public:
+    Figur(/* args */);
+    ~Figur();
+};
+
+Figur::Figur(/* args */)
+{
+}
+
+Figur::~Figur()
+{
+}
+
+int main()
+{
+
+    return 0;
+}
