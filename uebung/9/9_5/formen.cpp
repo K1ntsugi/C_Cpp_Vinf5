632118d068ed5cad5c255b087fcf24c5aa320c90
MaxGroetsch@lfst <max-groetsch@gmx.de>
2023-01-09 14:04:36 +0100

    uebung 9.5 done

--- uebung/9/9_5/formen.cpp
+++ uebung/9/9_5/formen.cpp
@@ -1,26 +1,61 @@
 #include <iostream>
+#include <cmath>
 
 using namespace std;
 
-class Figur
-{
-private:
-    /* data */
+class Figur {
 public:
-    Figur(/* args */);
-    ~Figur();
+    virtual double flaeche() const = 0;
+
+    virtual double umfang() const = 0;
 };
 
-Figur::Figur(/* args */)
-{
-}
+class Rechteck : public Figur {
+private:
+    double a, b; // Seiten
+public:
+    Rechteck(double A, double B) : a(A), b(B) {}
 
-Figur::~Figur()
-{
-}
+    double flaeche() const { return a * b; }
 
-int main()
-{
+    double umfang() const { return 2.0 * (a + b); }
+};
+
+class Dreieck : public Figur {
+private:
+    double a, b, c;
+public:
+    Dreieck(double A, double B, double C) : a(A), b(B), c(C) {}
+
+    double flaeche() const {
+        return 0.5 * c * ::sqrt((a * a) - (0.25 * (c * c)));
+    }
+
+    double umfang() const { return a + b + c; }
+};
+
+class Kreis : public Figur {
+private:
+    //const double M_PI = 3.14159265;
+    double r;
+public:
+    Kreis(double R) { r = R; }
+
+    double umfang() const { return 2. * M_PI * r; } // 2. => double Konstante
+    double flaeche() const { return M_PI * r * r; }
+};
+
+
+int main() {
+    Kreis k (5.);
+    cout << k.flaeche() << ' ' << k.umfang() << endl;
+
+    Rechteck recht(2,3);
+    cout << recht.flaeche() << ' ' << recht.umfang() << endl;
+
+    Dreieck d(2,2,2);
+    cout << d.flaeche() << ' ' << d.umfang() << endl;
+
 
     return 0;
 }
